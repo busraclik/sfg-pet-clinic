f593a0dc9f91fd7b158dbe276980ee611758a75d
busraclik <rabusracelik@gmail.com>
2024-09-09 17:22:53 +0300

    adding spring data jpa service for owners. closes #39

--- pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/model/Pet.java
@@ -12,7 +12,7 @@ public class Pet extends BaseEntity {
     @Column(name = "name")
     private String name;
     @ManyToOne
-    @Column(name = "pet_type")
+   // @Column(name = "pet_type")
     @JoinColumn(name = "type_id")
     private PetType petType;
     @ManyToOne
