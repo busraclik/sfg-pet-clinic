9a7674962e4a17e5871f180292cccad1e4f53317
busraclik <rabusracelik@gmail.com>
2024-09-12 23:08:49 +0300

    test

--- pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -43,10 +43,6 @@ public class OwnerSDJpaService implements OwnerService {
 
     @Override
     public Owner save(Owner object) {
-        System.out.println("####################");
-        System.out.println("####################");
-        System.out.println("####################");
-        System.out.println("####################");
        return ownerRepository.save(object);
     }
 
