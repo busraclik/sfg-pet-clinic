e47b786623efdacbcc882ab4242888e42d045aaa
busraclik <rabusracelik@gmail.com>
2024-09-16 17:42:20 +0300

    Add Project Lombok and Refactor. closes #49

--- pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/model/Visit.java
@@ -1,9 +1,14 @@
 package gru.springframework.sfgpetclinic.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "visits")
 public class Visit extends BaseEntity{
@@ -19,23 +24,4 @@ public class Visit extends BaseEntity{
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Pet getPet() {
-        return pet;
-    }
-
-    public void setPet(Pet pet) {
-        this.pet = pet;
-    }
 }
