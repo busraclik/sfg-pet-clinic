d6590796f25923529f6f9e96908819197cfd8bbc
busraclik <rabusracelik@gmail.com>
2024-09-17 21:36:41 +0300

    test

--- pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/gru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -17,6 +17,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
+    //test
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
